c9a75933dd57c1a40e8c9e4c3f4c4deeb62edfa0
Gnimuc <qiyupei@gmail.com>
2021-06-27 17:37:45 +0900

    Add `clang_CompilerInvocation_createFromCommandLine`
    For now, there is no method to access the `CompilerInvocation` private field of `ASTUnit`, so we expose this API and create the `CompilerInvocation` on our own.
    
    fix-up

--- include/CXCompilerInvocation.h
+++ include/CXCompilerInvocation.h
@@ -1,8 +1,9 @@
 #ifndef LIBCLANGEX_CXCOMPILERINVOCATION_H
 #define LIBCLANGEX_CXCOMPILERINVOCATION_H
 
-#include "clang-c/Platform.h"
 #include "CXError.h"
+#include "CXDiagnostic.h"
+#include "clang-c/Platform.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,6 +15,12 @@ CINDEX_LINKAGE CXCompilerInvocation
 clang_CompilerInvocation_create(CXInit_Error *ErrorCode);
 
 CINDEX_LINKAGE void clang_CompilerInvocation_dispose(CXCompilerInvocation CI);
+
+CINDEX_LINKAGE CXCompilerInvocation
+clang_CompilerInvocation_createFromCommandLine(
+    const char *source_filename, const char *const *command_line_args,
+    int num_command_line_args, CXDiagnosticsEngine Diags,
+    CXInit_Error *ErrorCode);
 
 #ifdef __cplusplus
 }
--- src/CXCompilerInvocation.cpp
+++ src/CXCompilerInvocation.cpp
@@ -1,5 +1,6 @@
 #include "CXCompilerInvocation.h"
 #include "clang/Frontend/CompilerInvocation.h"
+#include "clang/Frontend/Utils.h"
 #include <cstdio>
 
 CXCompilerInvocation clang_CompilerInvocation_create(CXInit_Error *ErrorCode) {
@@ -21,4 +22,32 @@ CXCompilerInvocation clang_CompilerInvocation_create(CXInit_Error *ErrorCode) {
 
 void clang_CompilerInvocation_dispose(CXCompilerInvocation CI) {
   delete static_cast<clang::CompilerInvocation *>(CI);
+}
+
+CXCompilerInvocation clang_CompilerInvocation_createFromCommandLine(
+    const char *source_filename, const char *const *command_line_args,
+    int num_command_line_args, CXDiagnosticsEngine Diags,
+    CXInit_Error *ErrorCode) {
+  std::unique_ptr<std::vector<const char *>> Args(
+      std::make_unique<std::vector<const char *>>());
+
+  Args->insert(Args->end(), command_line_args,
+               command_line_args + num_command_line_args);
+
+  CXInit_Error Err = CXInit_NoError;
+  std::unique_ptr<clang::CompilerInvocation> ptr =
+      clang::createInvocationFromCommandLine(
+          llvm::makeArrayRef(Args->data(), Args->data() + Args->size()),
+          static_cast<clang::DiagnosticsEngine *>(Diags));
+
+  if (!ptr) {
+    fprintf(stderr,
+            "LIBCLANGEX ERROR: failed to create `clang::CompilerInvocation`\n");
+    Err = CXInit_CanNotCreate;
+  }
+
+  if (ErrorCode)
+    *ErrorCode = Err;
+
+  return ptr.release();
 }
