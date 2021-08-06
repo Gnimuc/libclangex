fbf8fae11236532c805a720d393f7480b32a2921
Gnimuc <qiyupei@gmail.com>
2021-08-06 20:33:41 +0900

    Add wrappers for `DeclContext`

--- include/clang-ex/CXASTContext.h
+++ include/clang-ex/CXASTContext.h
@@ -63,6 +63,61 @@ CINDEX_LINKAGE bool clang_DeclGroupRef_isDeclGroup(CXDeclGroupRef DG);
 
 CINDEX_LINKAGE CXDecl clang_DeclGroupRef_getSingleDecl(CXDeclGroupRef DG);
 
+// DeclContext
+CINDEX_LINKAGE const char *clang_DeclContext_getDeclKindName(CXDeclContext DC);
+
+CINDEX_LINKAGE CXDeclContext clang_DeclContext_getParent(CXDeclContext DC);
+
+CINDEX_LINKAGE CXDeclContext clang_DeclContext_getLexicalParent(CXDeclContext DC);
+
+CINDEX_LINKAGE CXDeclContext clang_DeclContext_getLookupParent(CXDeclContext DC);
+
+CINDEX_LINKAGE CXASTContext clang_DeclContext_getParentASTContext(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isClosure(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isFunctionOrMethod(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isLookupContext(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isFileContext(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isTranslationUnit(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isRecord(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isNamespace(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isStdNamespace(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isInlineNamespace(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isDependentContext(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isTransparentContext(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isExternCContext(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_isExternCXXContext(CXDeclContext DC);
+
+CINDEX_LINKAGE bool clang_DeclContext_Equals(CXDeclContext DC, CXDeclContext DC2);
+
+CINDEX_LINKAGE CXDeclContext clang_DeclContext_getPrimaryContext(CXDeclContext DC);
+
+CINDEX_LINKAGE void clang_DeclContext_addDecl(CXDeclContext DC, CXDecl D);
+
+CINDEX_LINKAGE void clang_DeclContext_addDeclInternal(CXDeclContext DC, CXDecl D);
+
+CINDEX_LINKAGE void clang_DeclContext_addHiddenDecl(CXDeclContext DC, CXDecl D);
+
+CINDEX_LINKAGE void clang_DeclContext_removeDecl(CXDeclContext DC, CXDecl D);
+
+CINDEX_LINKAGE void clang_DeclContext_containsDecl(CXDeclContext DC, CXDecl D);
+
+CINDEX_LINKAGE void clang_DeclContext_dumpDeclContext(CXDeclContext DC);
+
+CINDEX_LINKAGE void clang_DeclContext_dumpLookups(CXDeclContext DC);
+
 // Decl
 CINDEX_LINKAGE CXSourceLocation_ clang_Decl_getLocation(CXDecl DC);
 
